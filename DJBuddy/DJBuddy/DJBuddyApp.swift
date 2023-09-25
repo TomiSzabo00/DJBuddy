2ebcef204bf73e4d0eb6a5946377742b885d55bd
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-09-25 11:58:52 +0200

    Add project to repo

---
+++ DJBuddy/DJBuddy/DJBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJBuddyApp.swift
+//  DJBuddy
+//
+//  Created by Szabó Tamás on 2023. 09. 25..
+//
+
+import SwiftUI
+
+@main
+struct DJBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
