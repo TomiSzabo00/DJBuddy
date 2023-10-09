de23d37d1556de639bf8c69715715b22fbdab75b
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-10-09 20:37:24 +0200

    Fix layout

--- DJBuddy/DJBuddy/Screens/ProfileView.swift
+++ DJBuddy/DJBuddy/Screens/ProfileView.swift
@@ -9,43 +9,46 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            Image("default")
-                .resizable()
-                .scaledToFit()
-                .overlay(
-                    LinearGradient(gradient: Gradient(colors: [.clear, .asset.background]),
-                                   startPoint: .top,
-                                   endPoint: .bottom)
-                )
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("DJ")
-                        .font(.system(size: 48))
-                        .fontWeight(.bold)
-                    Text(verbatim: "dj@email.com")
-                        .font(.callout)
+        GeometryReader { geo in
+            VStack(spacing: 0) {
+                Image("default")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geo.size.width, height: geo.size.width)
+                    .overlay(
+                        LinearGradient(gradient: Gradient(colors: [.clear, .asset.background]),
+                                       startPoint: .top,
+                                       endPoint: .bottom)
+                    )
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("DJ")
+                            .font(.system(size: 48))
+                            .fontWeight(.bold)
+                        Text(verbatim: "dj@email.com")
+                            .font(.callout)
+                    }
+                    .foregroundStyle(Color.white)
+
+                    Spacer()
+
+                    CircleButton {} label: {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .font(.title3)
+                            .foregroundStyle(Color.black)
+                    }
                 }
-                .foregroundStyle(Color.white)
+                .padding(.horizontal)
+                .offset(y: -30)
 
                 Spacer()
-
-                CircleButton {} label: {
-                    Image(systemName: "photo.on.rectangle.angled")
-                        .font(.title3)
-                        .foregroundStyle(Color.black)
-                }
             }
-            .padding(.horizontal)
-            .offset(y: -30)
-
-            Spacer()
-        }
-        .ignoresSafeArea(edges: .horizontal)
-        .navigationTitle("Profile")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarColor(backgroundColor: .black, titleColor: .red)
+            .ignoresSafeArea(edges: .horizontal)
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarColor(backgroundColor: .black, titleColor: .red)
         .backgroundColor(.asset.background)
+        }
     }
 }
 
